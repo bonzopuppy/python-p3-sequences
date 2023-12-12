b136277ff4d510ee2901813eed2d045a6e48d7b9
David Baden <davebaden@gmail.com>
2023-12-12 13:23:44 -0800

    pass tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
+# def test_print_fibonacci_zero(self):
+#         '''prints empty list when length = 0'''
+
+# def test_print_fibonacci_one(self):
+#         '''prints 0 when length = 1'''
+
+# def test_print_fibonacci_two(self):
+#         '''prints 0\\n1 when length = 2'''
+
+# def test_print_fibonacci_ten(self):
+#         '''prints 0\\n1\\n1\\n2\\n3\\n5\\n8\\n13\\n21\\n34 when length = 10'''
+
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        fibonacci = [0, 1]
+        for i in range(2, length):
+            fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])
+        print(fibonacci)
